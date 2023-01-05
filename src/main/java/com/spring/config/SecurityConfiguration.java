82924b6ab1e1b1ced23fa0ba14c24f61e5a8584b
mhmdyouali <mhmd.you@yahoo.com>
2023-01-06 00:46:27 +0200

    from database & noOpPasswordEncoder

--- src/main/java/com/spring/config/SecurityConfiguration.java
+++ src/main/java/com/spring/config/SecurityConfiguration.java
@@ -4,7 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
@@ -19,6 +28,36 @@ public class SecurityConfiguration {
                 .httpBasic(Customizer.withDefaults());
         
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) throws Exception{
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    /*@Bean
+    public UserDetailsService userDetailsService() throws Exception{
+        var admin = new InMemoryUserDetailsManager();
+        admin.createUser(
+                User.withUsername("anakin")
+                        .password(encoder().encode("anakin"))
+                        .authorities("ACCESS_ALL","ROLE_ADMIN")
+                        .build()
+        );
+
+        admin.createUser(
+                User.withUsername("anakin1")
+                        .password(encoder().encode("anakin1"))
+                        .authorities("ACCESS_ALL","ROLE_ADMIN")
+                        .build()
+        );
+
+        return admin;
+    }
+*/
+    @Bean
+    public NoOpPasswordEncoder encoder(){
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
 
 }
