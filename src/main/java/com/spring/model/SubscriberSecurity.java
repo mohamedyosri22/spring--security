c93f6864f67ac4049686f9039dd995d80ceafca8
mhmdyouali <mhmd.you@yahoo.com>
2023-01-06 18:02:08 +0200

    Custom Spring login

---
+++ src/main/java/com/spring/model/SubscriberSecurity.java
@@ -0,0 +1,55 @@
+package com.spring.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class SubscriberSecurity implements UserDetails {
+
+    private Subscriber subscriber;
+
+    public SubscriberSecurity(Subscriber subscriber) {
+        this.subscriber = subscriber;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority(subscriber.getRole()));
+        return authorityList;
+    }
+
+    @Override
+    public String getPassword() {
+        return subscriber.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return subscriber.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
