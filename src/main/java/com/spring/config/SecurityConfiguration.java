b0854fb3745d1eda94932b78d0cb7d7f70f7ff23
mhmdyouali <mhmd.you@yahoo.com>
2023-01-09 23:58:21 +0200

    CORS Configurations in SecurityFilterChain

--- src/main/java/com/spring/config/SecurityConfiguration.java
+++ src/main/java/com/spring/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.spring.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,6 +8,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Configuration
 public class SecurityConfiguration {
@@ -18,8 +24,21 @@ public class SecurityConfiguration {
                         "/swimming/*","/subs/*").authenticated()
                         .requestMatchers("/about/","/connect/").permitAll()
                 )
-                .httpBasic(Customizer.withDefaults());
-        
+                .httpBasic(Customizer.withDefaults())
+                .cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        var config = new CorsConfiguration();
+                        // angular website
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(2500L);
+                        return config;
+                    }
+                });
+
         return http.build();
     }
 
