229e9af6ce4b1c9eecd734e9458ae2aeb71c2c58
mhmdyouali <mhmd.you@yahoo.com>
2023-01-21 23:55:20 +0200

    JWT token filter

---
+++ src/main/java/com/spring/constants/SecurityConstant.java
@@ -0,0 +1,6 @@
+package com.spring.constants;
+
+public class SecurityConstant {
+    public static final String KEY = "meshomesho1";
+    public static final String HEADER = "Authorization";
+}
---
+++ src/main/java/com/spring/filters/JWTTokenFilter.java
@@ -0,0 +1,53 @@
+package com.spring.filters;
+
+import com.spring.constants.SecurityConstant;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+public class JWTTokenFilter extends OncePerRequestFilter {
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return !request.getServletPath().equals("/login");
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        Authentication authentication = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+        if(authentication != null){
+            SecretKey secretKey = Keys.hmacShaKeyFor(SecurityConstant.KEY.getBytes(StandardCharsets.UTF_8));
+            String jwt = Jwts.builder().setSubject("JWT Token")
+                    .claim("email",authentication.getName())
+                    .claim("authorities",authorities(authentication.getAuthorities()))
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(new Date().getTime() + 30000)) //ms
+                    .signWith(secretKey).compact();
+
+            response.setHeader(SecurityConstant.HEADER,jwt);
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    private String authorities(Collection<? extends GrantedAuthority> collections){
+        Set<String> auth = new HashSet<>();
+        for(GrantedAuthority authority : collections){
+            auth.add(authority.getAuthority());
+        }
+        return String.join(",",auth); //"USER,ADMIN" as String
+    }
+}
